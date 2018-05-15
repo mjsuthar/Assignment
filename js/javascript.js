69a6216f96b8c0e441236883e4ed68fa35136c0b
mjsuthar <mjkiid2@gmail.com>
2018-05-15 18:15:16 +0530

    Final changes

--- js/javascript.js
+++ js/javascript.js
@@ -1,59 +1,37 @@
-/* function validateEmail(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
-}
-
-function validate() {
-  var $result = $("#resultEmail");
-  var email = $("#email").val();
-  $result.text("");
-
-  if (validateEmail(email)) {
-    $result.text(email + " is valid ");
-    $result.css("color", "green");
-  } else {
-    $result.text(email + " is not valid ");
-    $result.css("color", "red");
-  }
-  return false;
-}
-
-$("#validate").bind("click", validate); */
 function Submit(){
 		var name = document.getElementById('name').value;
 		var email = document.getElementById('email').value;
 		var phone = document.getElementById('number').value;
 		var web = document.getElementById('web').value;
 		var DOB = document.getElementById('dob').value;
-		
 		var regWeb = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/; 
 		
 		// name validation
 		if(name == '' || name == null){
-			document.getElementById('resultName').innerHTML = " empty string";
+			document.getElementById('resultName').innerHTML = " Please Enter Name";
 		}else if(/^[A-Za-z\s]+$/.test(name)){
 			document.getElementById('resultName').innerHTML = name;
 		}else{
-			document.getElementById('resultName').innerHTML = " invalid name";
+			document.getElementById('resultName').innerHTML = " Invalid Name";
 		}
 		
 		//email validation
 		var pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		if(email == '' || email == null){
-			document.getElementById('resultEmail').innerHTML = " empty string email";
+			document.getElementById('resultEmail').innerHTML = " Please Enter Email";
 		}else if(pattern.test(email)){
 			document.getElementById('resultEmail').innerHTML = email;
 		}else{
-			document.getElementById('resultEmail').innerHTML = " invalid email";
+			document.getElementById('resultEmail').innerHTML = " Invalid Email";
 		}
 		
 		//Phone Number validation
 		if(phone == '' || phone == null){
-			document.getElementById('resultPhone').innerHTML = " empty";
+			document.getElementById('resultPhone').innerHTML = " Please Enter Phone Number given formate";
 		}else if(/^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/.test(phone)){
 			document.getElementById('resultPhone').innerHTML = phone;
 		}else{
-			document.getElementById('resultPhone').innerHTML = "invalid Phone No";
+			document.getElementById('resultPhone').innerHTML = "Invalid Phone No";
 		}
 		
 		//Web address validation
@@ -62,6 +40,34 @@ function Submit(){
 		}else if(regWeb.test(web)){
 			document.getElementById('resultWeb').innerHTML = web;
 		}else{
-			document.getElementById('resultWeb').innerHTML = " invalid Web url";
+			document.getElementById('resultWeb').innerHTML = " Invalid Web url";
 		}
+		
+		//Date of Birth validation
+		var d = new Date();
+		var year = Number(DOB.substr(0,4));
+		var month = Number(DOB.substr(5,2)) - 1;
+		var day = Number(DOB.substr(8,2));
+		if(DOB.length == 0){
+			document.getElementById('resultDOB').innerHTML = "Please insert Date of Birth";
+		}else if(year < d.getFullYear()){
+				document.getElementById('resultDOB').innerHTML = DOB;		
+		}else if(year == d.getFullYear()){
+			if(month < d.getMonth()){
+				document.getElementById('resultDOB').innerHTML = DOB;
+			}else if(month == d.getMonth()){
+				if(day <= d.getDate()){
+					document.getElementById('resultDOB').innerHTML = DOB;
+				}else{
+					document.getElementById('resultDOB').innerHTML = "Invalid Date";
+				}
+			}else{
+				document.getElementById('resultDOB').innerHTML = "Invalid Date";
+			}
+		}else{
+			document.getElementById('resultDOB').innerHTML = "Invalid Date";
+		}
+		document.getElementById("fillDetails").style.display = "none";
+		document.getElementById("outputs").style.display = "block";
+		
 	}
